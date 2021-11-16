351fae11d1136f2d5a0e7cf37829a51bc007500f
keepitclassick <keepitclassick@gmail.com>
2021-11-16 15:54:12 -0500

    char counter implemented

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
-  $(".new-tweet textarea").on("keydown", function()  {
-  let newTweetLength = $(this).val().length;
-  let closeCounter = $(this).siblings('.counter');
-  console.log("new",newTweetLength);
-  console.log("count",closeCounter);
+  $(".new-tweet textarea").on("input", function()  {
+    let tweetLength = $(this).val().length;
+    // Check if tweet length becomes greater than 140
+    let remainingLength = 140 - tweetLength;
+    // Update the counter to remaining length
+    $(".counter").text(remainingLength);
+    // Set the character counter to red if it goes below 0 by adding a class
+    if (remainingLength < 0) {
+        $(".counter").text(remainingLength).addClass("setRed");
+    } else {
+        $(".counter").text(remainingLength).removeClass("setRed");
+        // Refresh error message
+        $(".error-message").text("");
+    }      
   });
 });
